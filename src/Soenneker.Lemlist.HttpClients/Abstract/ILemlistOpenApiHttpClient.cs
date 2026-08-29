24e53d52079dceef8292274caad904185190e288
soenneker <jake@soenneker.com>
2026-08-29 14:17:08 -0500

    Improve public API XML documentation

--- src/Soenneker.Lemlist.HttpClients/Abstract/ILemlistOpenApiHttpClient.cs
+++ src/Soenneker.Lemlist.HttpClients/Abstract/ILemlistOpenApiHttpClient.cs
@@ -2,7 +2,6 @@ using System;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Threading;
-
 namespace Soenneker.Lemlist.HttpClients.Abstract;
 
 /// <summary>
@@ -10,5 +9,10 @@ namespace Soenneker.Lemlist.HttpClients.Abstract;
 /// </summary>
 public interface ILemlistOpenApiHttpClient: IDisposable, IAsyncDisposable
 {
+    /// <summary>
+    /// Returns the configured HTTP client used by the Lemlist OpenAPI HTTP Client.
+    /// </summary>
+    /// <param name="cancellationToken">Token used to cancel the operation.</param>
+    /// <returns>A task whose result is the requested HTTP client.</returns>
     ValueTask<HttpClient> Get(CancellationToken cancellationToken = default);
 }
